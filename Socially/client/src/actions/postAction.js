c10b00d223da0da54d9b7831891d53782e3a65dc
ankeett <94247776+ankeett@users.noreply.github.com>
2023-03-30 12:22:08 -0400

    show posts

--- Socially/client/src/actions/postAction.js
+++ Socially/client/src/actions/postAction.js
@@ -6,6 +6,8 @@ import {
   DELETE_A_POST_REQUEST, DELETE_A_POST_SUCCESS, DELETE_A_POST_FAIL,
 } from '../constants/postConstants';
 
+import { USER_DETAILS_REQUEST,USER_DETAILS_SUCCESS,USER_DETAILS_FAIL } from '../constants/userConstants';
+
 import axios from 'axios';
 
 const host = "http://localhost:4000";
@@ -20,8 +22,44 @@ export const createPost = (description,tags) => async (dispatch) => {
             withCredentials: true
         }
         const { data } = await axios.post(`${host}/api/posts`, {description,tags}, config);
-        dispatch({ type: CREATE_POST_SUCCESS, payload: data.post });
+        dispatch({ type: CREATE_POST_SUCCESS, payload: data });
     } catch (error) {
         dispatch({ type: CREATE_POST_FAIL, payload: error.response.data.message });
+    }
+}
+
+export const getAllPosts = () => async (dispatch) => {
+    try{
+        dispatch({type:FETCH_POSTS_REQUEST});
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        }
+        const {data} = await axios.get(`${host}/api/post/all`,config);
+        dispatch({type:FETCH_POSTS_SUCCESS,payload:data.posts});
+        console.log(data.posts)
+    }
+    catch(error){
+        dispatch({type:FETCH_POSTS_FAIL,payload:error.response.data.message});
+    }
+}
+
+export const userDetails = (id) => async (dispatch) => {
+    try{
+        dispatch({type:USER_DETAILS_REQUEST});
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        }
+        const {data} = await axios.get(`${host}/api/post/user/${id}`,config);
+        console.log(data)
+        dispatch({type:USER_DETAILS_SUCCESS,payload:data.user});
+    }
+    catch(error){
+        dispatch({type:USER_DETAILS_FAIL,payload:error.response.data.message});
     }
 }
--- Socially/client/src/components/Post/Feed.js
+++ Socially/client/src/components/Post/Feed.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Post from './Post'
-import {} from '../../actions/postAction'
+import {getAllPosts, userDetails} from '../../actions/postAction'
+import {useDispatch,useSelector} from 'react-redux'
 
 const Feed = () => {
+    const dispatch = useDispatch()
+    const {post} = useSelector(state=>state.posts)
+
+    useEffect(()=>{
+        dispatch(getAllPosts())
+    },[dispatch])
+    console.log(post)
+
+
+   
+
   return (
     <div className='mt-3 flex flex-col gap-3'>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+        {
+            post?.map((p)=>(
+                <Post key={p._id} p={p}/>
+            ))
+        }
     </div>
   )
 }
